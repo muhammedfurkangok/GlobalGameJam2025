d47061da25df7289414b665d3e3f56d3f258592d
Agah <agahgames260@gmail.com>
2025-01-26 14:36:05 +0300

    send

--- Assets/Scripts/Parallax/BackgroundController.cs
+++ Assets/Scripts/Parallax/BackgroundController.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    float startPos;
+    float startPos, length;
     public GameObject cam;
     public float parallaxEffect;
     
     void Start()
     {
         startPos = transform.position.x;
-
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float dist = (cam.transform.position.x * parallaxEffect);
+
         transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
 
     }
